5054debcdb6143c64090b79af4b46a670e9452f3
c13arm <allea10@live.se>
2014-05-19 07:56:40 +0200

    Update Request.java

--- src/routingAlgoritm/Request.java
+++ src/routingAlgoritm/Request.java
@@ -2,12 +2,35 @@ package routingAlgoritm;
 
 public class Request extends Message
 {
-
+	private Stack<Position> route = new Stack<Position>();
+	private int id;
+	private int maxNrOfJumps = 45;
+	private int nrOfJumps = 0;
+	private boolean knowRoute;
+	
 	public Request(int id)
 	{
 		super(0);
+		this.id = id;
 	}
-
 	
-
+	public void update()
+	{
+		nrOfJumps++;
+	}
+	
+	public Stack<Position> getRoute()
+	{
+		return route;
+	}
+	
+	public int getNrOfJumps()
+	{
+		return nrOfJumps;
+	}
+	
+	public int getId()
+	{
+		return id;
+	}
 }
